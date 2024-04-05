73ccd0b8a6a2d1dcb1d4b87c51198634b649b533
Paulo Vinícius <pauloviniciusmc5@gmail.com>
2024-04-05 19:01:51 -0300

    FIX home e PostInd
    Também comentei o blog.views

--- apps/home/views.py
+++ apps/home/views.py
@@ -4,6 +4,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    posts = Post.objects.all().order_by('-acessos')
-    return render(request, 'index.html', {posts: posts})
-    #return HttpResponse(posts[0].titulo+'\n'+posts[1].titulo+'\n'+posts[2].titulo)
+    return render(request, 'index.html')
