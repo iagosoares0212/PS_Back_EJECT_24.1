7881de7148c27158a58f526d6797502f2d458319
Paulo Vinícius <65673077+Paulo-Vinicius-m@users.noreply.github.com>
2024-04-07 16:28:32 -0300

    fix bug deploy
    conflito django 5 vs django 4

--- apps/contato/models.py
+++ apps/contato/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Contato (models.Model):
-    ASSUNTO_CHOICES = {
-        'a': 'Ajuda',
-        'e': 'Elogio',
-        's': 'Sugestão'
-        }
+    ASSUNTO_CHOICES = [
+        ('a', 'Ajuda'),
+        ('e', 'Elogio'),
+        ('s', 'Sugestão')
+    ]
     
     assunto = models.CharField(max_length=1, choices=ASSUNTO_CHOICES)
     nome = models.CharField(max_length=100)
@@ -15,4 +15,4 @@ class Contato (models.Model):
     data = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.ASSUNTO_CHOICES[self.assunto]
+        return self.assunto
